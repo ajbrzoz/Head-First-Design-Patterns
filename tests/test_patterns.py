b6cd370aa0460d084330e95e8dc4634808ac4b85
ajbrzoz <brzozowskaanna5@gmail.com>
2017-09-07 15:50:04 +0200

    Observer tests added

--- tests/test_patterns.py
+++ tests/test_patterns.py
@@ -5,6 +5,9 @@ import sys
 from strategy.ducks import MallardDuck, ModelDuck
 from strategy.fly_behaviours import FlyRocketPowered
 
+from observer.weather_data import WeatherData
+from observer.observers import CurrentConditionsDisplay
+
 class StrategyTestCase(unittest.TestCase):
     
     def setUp(self):
@@ -37,3 +40,21 @@ class StrategyTestCase(unittest.TestCase):
         self.model.perform_fly()
         sys.stdout = sys.__stdout__
         self.assertEqual(self.captured.getvalue().strip(), "I'm flying with a rocket!")
+
+
+class ObserverTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.w_data = WeatherData()
+        self.current_display = CurrentConditionsDisplay(self.w_data)
+        
+    def test_changing_measurements(self):
+        """Test if the subject's and observers' values are equal"""
+        temperature_values = [80, 82, 78]
+        humidity_values = [65, 70, 90]
+        pressure_values = [30.4, 29.2, 29.2]
+        
+        for t, h, p in zip(temperature_values, humidity_values, pressure_values):
+            self.w_data.set_measurements(t, h, p)
+            self.assertEqual(self.current_display.temperature, t)
+            self.assertEqual(self.current_display.humidity, h)
